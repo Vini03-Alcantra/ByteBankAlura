c88aede518245c263944c83bea97a46e02d20511
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-07-04 01:20:38 -0300

    criando classe contact_form para cadastrar novos contatos

--- bytebankalura/lib/views/home.dart
+++ bytebankalura/lib/views/home.dart
@@ -1,10 +1,11 @@
 import 'package:bytebankalura/views/contatos.dart';
+import 'package:bytebankalura/views/dashboard.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ContatosLista();
+    return Dashboard();
   }
 }
 
