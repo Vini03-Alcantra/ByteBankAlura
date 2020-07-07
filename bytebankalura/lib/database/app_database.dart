f6530c190158dc9b1be2f2d023c505e6c89dc5df
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-07-06 21:54:16 -0300

    Retorno no create database

--- bytebankalura/lib/database/app_database.dart
+++ bytebankalura/lib/database/app_database.dart
@@ -1,14 +1,17 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
-void createDatabase(){
-  getDatabasesPath().then((dbPath){
+
+Future<Database> createDatabase(){
+  //Função para buscar caminho do banco
+  return getDatabasesPath().then((dbPath){
+    //função join cria o banco no local esperado (caminho, nome_do_banco)
     final String path = join(dbPath, 'bytebank.db');
-    openDatabase(path, onCreate: (db, version){
+    return openDatabase(path, onCreate: (db, version){
       db.execute(
-        'CREATE TABLE contatos('
-          'id INTEGER PRIMARY KEY,' 
-          'nome TEXT,'
-          'numero_conta INTEGER)'
+        'CREATE TABLE contatos( '
+          'id INTEGER PRIMARY KEY, ' 
+          'nome TEXT, '
+          'numero_conta INTEGER) '
         );        
       }, version: 1
     );
