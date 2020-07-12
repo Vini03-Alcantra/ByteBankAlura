0d8b35af1ad42bf9d79135b65d9ed46498f1fd6c
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-07-11 22:58:06 -0300

    async e awaitno método save

--- bytebankalura/lib/database/app_database.dart
+++ bytebankalura/lib/database/app_database.dart
@@ -2,7 +2,7 @@ import 'package:bytebankalura/models/contato.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-Future<Database> createDatabase() async {
+Future<Database> getDatabase() async {
   //função join cria o banco no local esperado (caminho, nome_do_banco)
   final String path = join(await getDatabasesPath(), 'bytebank.db');
   return openDatabase(path, onCreate: (db, version){
@@ -19,17 +19,17 @@ Future<Database> createDatabase() async {
   
 
 
-Future<int> save(Contato contato){
-  return createDatabase().then((value){
+Future<int> save(Contato contato) async {
+    final Database db = await getDatabase();
     final Map<String, dynamic> contatoMap = Map();    
     contatoMap['nome'] = contato.name;
     contatoMap['numero_conta'] = contato.numeroConta;
-    return value.insert("contatos", contatoMap);
-  });
+    return db.insert('contatos', contatoMap);
+  
 }
 
 Future<List<Contato>> findAll(){
- return createDatabase().then((db){
+ return getDatabase().then((db){
     return db.query('contatos').then((maps){
       final List<Contato> contatos = List();
       for (Map<String, dynamic> map in maps) {
