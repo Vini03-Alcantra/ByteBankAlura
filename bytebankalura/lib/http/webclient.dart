240aa7786433434931253a8840dbf5c34c1301c7
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-07-12 01:55:40 -0300

    Adicionando funcionalidades do logginginterceptor

--- bytebankalura/lib/http/webclient.dart
+++ bytebankalura/lib/http/webclient.dart
@@ -1,7 +1,22 @@
-import 'package:flutter/material.dart';
-
+import 'package:http_interceptor/http_interceptor.dart';
 import 'package:http/http.dart';
 
+class LoggingInterceptor implements InterceptorContract{
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print(data);
+    return data;
+  }
+
+  @override 
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print(data);
+    return data;
+  }
+}
+
+
 void findAll() async {
+  final Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
   final Response response = await get('http://localhost:8080/transactions');
 }
