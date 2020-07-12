36542bc0df0106a4e04a5d4bcc68e0aa03559940
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-07-12 00:02:58 -0300

    Adicionando novo card para transação'

--- bytebankalura/lib/views/dashboard.dart
+++ bytebankalura/lib/views/dashboard.dart
@@ -17,37 +17,51 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('imagens/bytebank_logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Colors.green[500],
-              child: InkWell(
-                onTap: (){
-                  return Navigator.push(context, MaterialPageRoute(builder: (context) => ContatosLista()));
-                },
-                child: Container(  
-                  padding: EdgeInsets.all(8.0),
-                  height: 100,
-                  width: 150,
-                  color: Colors.green,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(Icons.people, color: Colors.white, size: 26,),
-                      Text("Contatos", 
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0
-                      ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
+          Row(
+            children: <Widget>[
+              _FeatureItem(nome: "Transfet", icone: Icons.monetization_on,),
+              _FeatureItem(nome: "linha de Transação", icone: Icons.description,)
+            ],
           )
+          
         ],
+      ),
+    );
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  final String nome;
+  final IconData icone;
+
+  const _FeatureItem({Key key, this.nome, this.icone}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Colors.green[500],
+        child: InkWell(
+          onTap: (){
+            return Navigator.push(context, MaterialPageRoute(builder: (context) => ContatosLista()));
+          },
+          child: Container(  
+            padding: EdgeInsets.all(8.0),
+            height: 100,
+            width: 150,
+            color: Colors.green,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(padding: const EdgeInsets.all(8.0)),
+                Icon(icone),
+                Text(nome)
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
