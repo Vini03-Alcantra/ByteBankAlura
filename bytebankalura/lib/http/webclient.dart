63134cf0decab092d20f559229afc65741304f31
Vinícius de Alcantra <viniciusalcantarabarros@gmail.com>
2020-07-12 16:38:51 -0300

    fazendo get de api do localhost

--- bytebankalura/lib/http/webclient.dart
+++ bytebankalura/lib/http/webclient.dart
@@ -1,6 +1,8 @@
 import 'package:http_interceptor/http_interceptor.dart';
 import 'package:http/http.dart';
-
+import 'dart:convert';
+import 'package:bytebankalura/models/transaction.dart';
+import 'package:bytebankalura/models/contato.dart';
 class LoggingInterceptor implements InterceptorContract{
   @override
   Future<RequestData> interceptRequest({RequestData data}) async {
@@ -16,7 +18,19 @@ class LoggingInterceptor implements InterceptorContract{
 }
 
 
-void findAll() async {
+Future<List<Transaction>> findAll() async {
   final Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
-  final Response response = await get('http://localhost:8080/transactions');
+  final Response response = await client.get('http://192.168.0.13:8080/transactions');
+  final List<dynamic> decodedJson = jsonDecode(response.body);
+  final List<Transaction> transactions = List();
+  for (Map<String, dynamic> element in decodedJson) {
+    final Transaction transaction = Transaction(      
+      value: element['value'],
+      contato: Contato(0, element['contact']['name'], element['contact']['accountNumber'])
+    );
+    print('decoded Json $decodedJson');
+    transactions.add(transaction);
+  }
+
+  return transactions;
 }
--- bytebankalura/lib/main.dart
+++ bytebankalura/lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:bytebankalura/http/webclient.dart';
 import 'package:bytebankalura/views/home.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MaterialApp(
+void main(){
+  runApp(
+    MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Home()
   )
-);
+  );
+
+  findAll().then((value) => print("Transações ${value.toString()}"));
+}
